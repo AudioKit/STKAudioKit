f90edf0a30f1bae7ff94eaabb37c8fce0f46591a
Taylor Holliday <taylor@audulus.com>
2021-05-20 19:43:10 -0700

    attempt to fix CI by creating a dummy header

---
+++ Sources/CSTKAudioKit/include/CSTKAudioKit.h
@@ -0,0 +1,8 @@
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
+
+#ifndef CSTKAudioKit_h
+#define CSTKAudioKit_h
+
+// CSTKAudioKit has no public interface. It registers DSP kernels automatically.
+
+#endif /* CSTKAudioKit_h */
