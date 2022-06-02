1451df4815b788fecdf090e5de52b81f6b5a8863
Aurelius Prochazka <aurephd@gmail.com>
2022-06-01 19:26:28 -0700

    Added a playground page

---
+++ Playgrounds/STKAudioKit.playground/Pages/Clarinet.xcplaygroundpage/Contents.swift
@@ -0,0 +1,34 @@
+//: ## Clarinet
+//: Physical model of a Clarinet
+import AudioKit
+import STKAudioKit
+import Foundation
+import PlaygroundSupport
+
+let playRate = 2.0
+
+let clarinet = Clarinet()
+
+let reverb = Reverb(clarinet)
+
+let scale = [0, 2, 4, 5, 7, 9, 11, 12]
+
+
+let engine = AudioEngine()
+engine.output = reverb
+try! engine.start()
+
+let t = Timer.scheduledTimer(withTimeInterval: 1.0 / playRate, repeats: true) { _ in
+    var note = scale.randomElement()!
+    let octave = (2..<6).randomElement()! * 12
+    if (0...10).randomElement()! < 1 { note += 1 }
+    if !scale.contains(note % 12) { Log("ACCIDENT!") }
+
+    if (0...6).randomElement()! > 1 {
+        clarinet.trigger(note: MIDINoteNumber(note + octave), velocity: 127)
+    } else {
+        clarinet.stop()
+    }
+}
+
+PlaygroundPage.current.needsIndefiniteExecution = true
